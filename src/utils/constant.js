97bd793809ff5aeb7ff4bea573e522075231ee29
mickwif <mickweb3@proton.me>
2023-03-08 12:32:25 +0800

    update default logo for dao

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,4 +3,4 @@ loadEnv();
 
 export const TonBot = process.env.TON_BOT;
 
-export const DEFAULT_DAO_LOGO = "https://i.328888.xyz/2023/03/07/54dJc.jpeg";
+export const DEFAULT_DAO_LOGO = "https://i.328888.xyz/2023/03/08/5KI9z.png";
